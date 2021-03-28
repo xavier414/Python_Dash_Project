324f9e778dae7576c70f2cc48add7ca18a3ca813
xavier414 <xavierbryant@shaw.ca>
2021-03-28 19:08:30 +0200

    little changes

--- app.py
+++ app.py
@@ -13,17 +13,30 @@ app = dash.Dash(
     __name__, title="White Wine Quality Dash - Volatile and Fixed Acidity")
 
 markdown_text = '''
-This data set assesses the quality of 4898 white wine variants from the Portuguese Vinho Verde region based on 11 physicochemical features. The region
-is in the northwest of Portugal as shown in the adjacent map to the left. The data was originally used in the paper [Modeling wine preferences by data mining from physicochemical properties](https://www.sciencedirect.com/science/article/abs/pii/S0167923609001377?via%3Dihub)
-by Cortez et al. (2009). The data set is posted on the [UCI Machine Learning Repository](https://archive.ics.uci.edu/ml/datasets/wine+quality), but was sourced in this project from [data.world.](https://data.world/food/wine-quality)
+This data set assesses the quality of 4898 white wine variants from the Portuguese Vinho Verde region based on 11 physicochemical features. The data was originally used in the paper [Modeling wine preferences by data mining from physicochemical properties](https://www.sciencedirect.com/science/article/abs/pii/S0167923609001377?via%3Dihub)
+by Cortez et al. (2009). The data set was posted originally on the [UCI Machine Learning Repository](https://archive.ics.uci.edu/ml/datasets/wine+quality), but was sourced in this project from [data.world.](https://data.world/food/wine-quality)
 
 The physicochemical properties of the white wine variants that act as the input variables:
 - Fixed acidity
 - Volatile acidity
 - Citric acid
 - Residual sugar
-- etc...
+- Chlorides
+- Free sulfur dioxide
+- Total sulfur dioxide
+- Density
+- pH
+- Alcohol
+
+The output variable is *quality* on a scale of 1 to 10, however values 0,1,2 and 10 do not - indicating there is no wine that is of perfect or no quality.
+
+In this analysis, there is a table which can be filtered by mulitple qualities of wine. There is also range slider to adjust the range reviewed for the fixed acidity varaible.
+An interactive scatterplot is also provided in another tab where you can see the fixed acidity plotted against the volatile acidity of wine varieties. The data is coloured 
+by quality. The range slider can also be used in the graph as well and the qualities can be selected too. In addition, the lasso feature can be used on the scatter plot to
+select data points and all the physicochemical properties can be seen for those data points selected below the plot.\
+
 '''
+
 df_url = 'https://query.data.world/s/tmlt63lm3n3uzb2ujhlmkarlzoeo73'
 df = pd.read_csv(df_url)
 
